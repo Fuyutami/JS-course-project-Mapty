519a89bed37a8715c4f87cb0bc6ab54f954e7ea9
[Fuyutami] <fuyutami.art@gmail.com>
2021-08-11 16:50:56 +0300

    display a map with leaflet

--- script.js
+++ script.js
@@ -16,7 +16,28 @@ if(navigator.geolocation)
 navigator.geolocation.getCurrentPosition(function (position) {
     const {latitude} = position.coords
     const {longitude} = position.coords
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+    const coords = [latitude, longitude]
+    
+    const map = L.map('map').setView(coords, 13); // we need HTML element with an ID of "map"
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    
+    map.on('click', function (mapEvent) {
+        console.log(mapEvent)
+        const {lat, lng} = mapEvent.latlng
+
+        L.marker([lat, lng], 'riseOnHover').addTo(map).bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        })).setPopupContent('Workout').openPopup();
+    })
+
 }, function () {
     alert('Could not get your position')
 })
