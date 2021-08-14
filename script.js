8a5b2f9bd038517b60058db6e73026b7ee413d00
[Fuyutami] <fuyutami.art@gmail.com>
2021-08-14 14:47:33 +0300

    move to marker on click

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let map, mapEvent;
 class Workout {
     date = new Date()
     id = (Date.now() + '').slice(-10)
+    clicks = 0
 
     constructor(coords, distance, duration) {
         this.coords = coords
@@ -28,6 +29,10 @@ class Workout {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on  ${months[this.date.getMonth()]} ${this.date.getDate()}`
+    }
+
+    click() {
+        this.clicks++
     }
 }
 
@@ -71,6 +76,7 @@ class Cycling extends Workout {
 // APPLICATION ARCHITECTURE
 class App {
     #map
+    #mapZoomLevel = 13
     #mapEvent
     #workouts = []
 
@@ -80,6 +86,8 @@ class App {
         form.addEventListener('submit', this._newWorkout.bind(this))
         
         inputType.addEventListener('change', this._toggleElevationField)
+
+        containerWorkouts.addEventListener('click', this._moveToPopup.bind(this))
     }
 
     _getPosition() {
@@ -94,7 +102,7 @@ class App {
         const {longitude} = position.coords
         const coords = [latitude, longitude]
         
-        this.#map = L.map('map').setView(coords, 13); // we need HTML element with an ID of "map"
+        this.#map = L.map('map').setView(coords, this.#mapZoomLevel); // we need HTML element with an ID of "map"
     
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -229,6 +237,26 @@ class App {
         `;
 
     form.insertAdjacentHTML('afterend', html)
+    }
+
+    _moveToPopup(e) {
+        const workoutEl = e.target.closest('.workout')
+        console.log(workoutEl)
+        
+        if(!workoutEl) return
+
+        const workout = this.#workouts.find(work => work.id === workoutEl.dataset.id)
+
+        console.log(workout)
+
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1
+            }
+        })
+
+        workout.click()
     }
 }
 
